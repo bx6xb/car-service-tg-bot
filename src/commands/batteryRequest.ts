c8589c4ba038c40d8ca8ea8008d031b6313ccf52
Yan Turnt <bx6xbchosen1@gmail.com>
2026-05-24 17:48:02 +0500

    feat: removed batter request logic

--- src/commands/batteryRequest.ts
+++ src/commands/batteryRequest.ts
@@ -1,12 +1,34 @@
 import { Markup } from 'telegraf';
 import { bot } from '../config';
-import { requestSteps, textState } from './state';
+import { editMessageText } from '../lib';
+
+// === СТАРАЯ ЛОГИКА (подбор через Telegram-бота) ===
+// Раскомментировать и удалить новый блок ниже, если возвращаемся к подбору внутри бота.
+//
+// import { requestSteps, textState } from './state';
+//
+// bot.action('battery_request', async (ctx) => {
+//   await ctx.answerCbQuery();
+//   const userId = ctx.from?.id;
+//   textState.set(userId, 'battery_request');
+//   requestSteps.set(userId, { step: 'car_brand' });
+//   await ctx.reply('Введите марку автомобиля', Markup.removeKeyboard());
+// });
+// === КОНЕЦ СТАРОЙ ЛОГИКИ ===
 
 bot.action('battery_request', async (ctx) => {
   await ctx.answerCbQuery();
-  const userId = ctx.from?.id;
 
-  textState.set(userId, 'battery_request');
-  requestSteps.set(userId, { step: 'car_brand' });
-  await ctx.reply('Введите марку автомобиля', Markup.removeKeyboard());
+  await editMessageText(
+    ctx,
+    '🔋 <b>Подбор аккумулятора</b>\n\nПодбор аккумуляторов теперь осуществляется через бота в мессенджере <b>Max</b> или через наш <b>сайт</b>.\n\nВыберите удобный способ 👇',
+    {
+      parse_mode: 'HTML',
+      ...Markup.inlineKeyboard([
+        [Markup.button.url('📱 Бот в Max', 'https://max.ru/id615426315675_bot')],
+        [Markup.button.url('🌐 Сайт ampercenter.ru', 'https://ampercenter.ru/')],
+        [Markup.button.callback('↩️ Назад', 'menu_main')],
+      ]),
+    },
+  );
 });
