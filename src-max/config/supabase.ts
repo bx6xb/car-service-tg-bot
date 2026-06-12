8160ff1b5cf3d24421bb8d26bf43bd70608e5930
Yan Turnt <bx6xbchosen1@gmail.com>
2026-06-12 13:34:31 +0500

    fix: ws in supabase

--- src-max/config/supabase.ts
+++ src-max/config/supabase.ts
@@ -2,10 +2,10 @@ import { createClient } from '@supabase/supabase-js';
 import ws from 'ws';
 import { MAX_SUPABASE_KEY, MAX_SUPABASE_URL, TG_SUPABASE_KEY, TG_SUPABASE_URL } from './variables';
 
-// Фикс для Node.js < 22
-if (!global.WebSocket) {
-  (global as any).WebSocket = ws;
-}
+export const supabase = createClient(MAX_SUPABASE_URL, MAX_SUPABASE_KEY, {
+  realtime: { transport: ws as any }
+});
 
-export const supabase = createClient(MAX_SUPABASE_URL, MAX_SUPABASE_KEY);
-export const tgSupabase = createClient(TG_SUPABASE_URL, TG_SUPABASE_KEY);
+export const tgSupabase = createClient(TG_SUPABASE_URL, TG_SUPABASE_KEY, {
+  realtime: { transport: ws as any }
+});
