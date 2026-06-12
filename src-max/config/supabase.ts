a3d959c07226909dc9c33e7652c7187cb7b359d6
Yan Turnt <bx6xbchosen1@gmail.com>
2026-06-12 13:31:46 +0500

    fix: types for ws and check ws

--- src-max/config/supabase.ts
+++ src-max/config/supabase.ts
@@ -1,5 +1,11 @@
 import { createClient } from '@supabase/supabase-js';
+import ws from 'ws';
 import { MAX_SUPABASE_KEY, MAX_SUPABASE_URL, TG_SUPABASE_KEY, TG_SUPABASE_URL } from './variables';
+
+// Фикс для Node.js < 22
+if (!global.WebSocket) {
+  (global as any).WebSocket = ws;
+}
 
 export const supabase = createClient(MAX_SUPABASE_URL, MAX_SUPABASE_KEY);
 export const tgSupabase = createClient(TG_SUPABASE_URL, TG_SUPABASE_KEY);
